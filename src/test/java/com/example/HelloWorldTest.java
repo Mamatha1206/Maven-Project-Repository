dbc72e11f993b11b86b448d2abcb1d892496f3fe
Mamatha Eudulakanti <119730722+Mamatha1206@users.noreply.github.com>
2025-05-03 14:03:29 +0530

    Create HelloWorldTest.java

---
+++ src/test/java/com/example/HelloWorldTest.java
@@ -0,0 +1,12 @@
+// maven-project/src/test/java/com/example/HelloWorldTest.java
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    public void testHelloWorld() {
+        assertEquals("Hello, World!", "Hello, World!");  // Example test
+    }
+}
